d5044a7829f185eb115b6dd3068f800304d0c8b9
dishabarmola <120310851+dishabarmola@users.noreply.github.com>
2023-01-26 01:48:13 +0530

    Create q5_a5.cpp

---
+++ q5_a5.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,k;
+    cin>>a;
+    if(a%2!=0){
+            k= (a+1)/2;
+            for(int i = 1; i<=a;i++){
+                if(i<=k){
+                    for(int j= 1;j<=a;j++){
+                        if(j>=(k-i+1)&&j<=(k+i-1)){
+                            cout<<" * ";
+                            
+                        }
+                        else{
+                            cout<<"   ";
+                            
+                        }
+                        
+                    }
+                    
+                }
+                else{
+                    for(int m=1;m<=a;m++){
+                        if(m>=(i-k+1)&&m<=((2*a)+1-k-i)){
+                            cout<<" * ";
+                            
+                        }
+                        else{
+                            cout<<"   ";
+                            
+                        }
+                    }
+                }
+                cout<<endl;
+        
+            }
+    
+
+    }   
+    
+
+    return 0;
+}
